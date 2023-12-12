066567b120453247acc200025d2f6129347bef67
Alex <prueba>
2023-12-12 10:55:33 +0100

    try catch

--- pages/suplementos.js
+++ pages/suplementos.js
@@ -29,22 +29,30 @@ const Suplementos = ({ suplementos }) => {
   );
 };
 
-export default Suplementos;
+
+
+
 
 export async function getStaticProps() {
   try {
     const respuesta = await fetch(
       `${process.env.API_URL}/suplementos?populate=imagen`
     );
+    
+    if (!respuesta.ok) {
+      throw new Error(`No se pudo obtener la lista de suplementos. Status: ${respuesta.status}`);
+    }
+
     const { data: suplementos } = await respuesta.json();
 
     return {
       props: {
         suplementos,
       },
+      revalidate: 60,
     };
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error("Error fetching suplementos:", error);
     return {
       props: {
         suplementos: [],
@@ -53,4 +61,34 @@ export async function getStaticProps() {
     };
   }
 }
+
+
+
+
+
+
+// export default Suplementos;
+
+// export async function getStaticProps() {
+//   try {
+//     const respuesta = await fetch(
+//       `${process.env.API_URL}/suplementos?populate=imagen`
+//     );
+//     const { data: suplementos } = await respuesta.json();
+
+//     return {
+//       props: {
+//         suplementos,
+//       },
+//     };
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     return {
+//       props: {
+//         suplementos: [],
+//       },
+//       revalidate: 60,
+//     };
+//   }
+// }
 
